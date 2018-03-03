1e7012ea0c110a45b7e9f2c0c52f59ab42f67ce4
Sameer Sarmah <sameer.paran.sarmah@sap.com>
2018-03-03 17:19:50 +0530

    A UI made from scratch using Typescript,HTML,CSS,jQuery and bundled using webpack to fetch data from north wind server using pagination.
    A UI made from scratch using Typescript,HTML,CSS,jQuery and bundled using webpack to fetch data from north wind server using pagination.

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+
+/**
+ * Created by matt on 2/19/17.
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const ExtractCSS= require('extract-text-webpack-plugin');
+module.exports = {
+  devtool: 'source-map',
+  context: path.join(__dirname, 'src'),
+  entry: {
+    product: './app/product.component.ts',
+    vendor:"jquery"
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        //use: ['style-loader','css-loader'],
+        use :ExtractCSS.extract({	
+        fallback: "style-loader",
+				use: {
+					loader: "css-loader",
+					options: {
+						sourceMap: true
+					}
+				}}),
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.(jpg)$/,
+        use: ['file-loader'],
+        include: path.join(__dirname, 'src')
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    inline: true,
+    hot: true,
+    historyApiFallback: true
+
+  },
+  resolve:{
+    extensions: ['.ts', '.js', '.json'],
+    alias:{
+      "styles": path.resolve(__dirname, 'src/styles'),
+      "assets": path.resolve(__dirname, 'src/assets'),
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      inject: 'body',
+      chunks: ['common','vendor','product'],
+      filename: 'index.html'
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ["common","bootstrap","vendor"]
+    }),
+    new ExtractCSS({
+			filename: "[name].css"
+    })
+    //,new Uglify()
+  ]
+};
